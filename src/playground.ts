2a19b42483424c06b73e2feb4f1e6d652703f11c
Carles Serra Vendrell <carles.serra.vendrell@hp.com>
2023-04-07 21:25:51 +0200

    feat: some easy solutions

--- src/playground.ts
+++ src/playground.ts
@@ -1,4 +1,12 @@
-import { MyPick, MyReadonly, TupleToObject } from "./easy";
+import {
+  First,
+  Length,
+  MyExclude,
+  MyPick,
+  MyReadonly,
+  TupleToObject,
+  MyAwaited,
+} from "./easy";
 
 // Pick
 interface Todo {
@@ -25,7 +33,9 @@ const todoReadonly: MyReadonly<TodoReadonly> = {
   description: "foobar",
 };
 
+// @ts-expect-error
 todoReadonly.title = "Hello"; // Error: cannot reassign a readonly property
+// @ts-expect-error
 todoReadonly.description = "barFoo"; // Error: cannot reassign a readonly property
 
 // Tuple to object
@@ -42,3 +52,32 @@ const todo: result = {
   "model X": "model X",
   "model Y": "model Y",
 };
+
+// First of array
+type arr1 = ["a", "b", "c"];
+type arr2 = [3, 2, 1];
+
+type head1 = First<arr1>; // expected to be 'a'
+type head2 = First<arr2>; // expected to be 3
+type head3 = First<[]>; // expected to be never
+
+// Length of tuple
+type tesla = ["tesla", "model 3", "model X", "model Y"];
+type spaceX = [
+  "FALCON 9",
+  "FALCON HEAVY",
+  "DRAGON",
+  "STARSHIP",
+  "HUMAN SPACEFLIGHT"
+];
+
+type teslaLength = Length<tesla>; // expected 4
+type spaceXLength = Length<spaceX>; // expected 5
+
+// Exclude
+type ExcludedResult = MyExclude<"a" | "b" | "c", "a">; // 'b' | 'c'
+
+// Awaited
+type ExampleType = Promise<string>;
+
+type AwaitedResult = MyAwaited<ExampleType>; // string
